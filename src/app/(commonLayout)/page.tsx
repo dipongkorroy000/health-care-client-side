210fce8442ac6b6d2afae1939a72c24ad958ca46
dipongkorroy000 <dipongkorroy000@gmail.com>
2025-11-06 15:46:15 +0600

    navbar task completed

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="border m-20 p-5">
       <h1>To get started, edit the page.tsx file.</h1>
       <Button>Click me</Button>
     </div>
