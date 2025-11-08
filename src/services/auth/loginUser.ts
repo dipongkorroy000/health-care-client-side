24961cbf57f91515899f4e3e56b1208dd2927c71
dipongkorroy000 <dipongkorroy000@gmail.com>
2025-11-08 11:36:03 +0600

    cookie set tasks completed

--- src/services/auth/loginUser.ts
+++ src/services/auth/loginUser.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
-
+import { parse } from "cookie";
+import { cookies } from "next/headers";
 import z from "zod";
 
 const loginValidationZodSchema = z.object({
@@ -27,12 +28,52 @@ export const loginUser = async (_currentState: any, formData: any): Promise<any>
       };
     }
 
-    return await fetch("http://localhost:5000/api/v1/auth/login", {
+    const res = await fetch("http://localhost:5000/api/v1/auth/login", {
       method: "POST",
       body: JSON.stringify(loginData),
       headers: { "Content-Type": "application/json" },
-    }).then((res) => res.json());
+    });
+
+    const result = await res.json();
+
+    const setCookieHeaders = res.headers.getSetCookie();
+
+    let accessTokenObj: null | any = null;
+    let refreshTokenObj: null | any = null;
+    // cookie parse by cookie npm package using
+    if (setCookieHeaders && setCookieHeaders.length > 0) {
+      setCookieHeaders.forEach((cookie: string) => {
+        const parsedCookie = parse(cookie);
+
+        if (parsedCookie["accessToken"]) accessTokenObj = parsedCookie;
+        if (parsedCookie.refreshToken) refreshTokenObj = parsedCookie;
+      });
+    }
+
+    // console.log("setCookie", accessTokenObj["Max-Age"], refreshTokenObj);
+
+    if (!accessTokenObj || !refreshTokenObj) throw new Error("No Set-Cookie header found");
+
+    (await cookies()).set("accessToken", accessTokenObj.accessToken, {
+      secure: true,
+      httpOnly: true,
+      maxAge: parseInt(accessTokenObj["Max-Age"]),
+      path: accessTokenObj.Path || "/",
+      sameSite: accessTokenObj.SameSite,
+    });
+
+    (await cookies()).set("refreshToken", refreshTokenObj.refreshToken, {
+      secure: true,
+      httpOnly: true,
+      maxAge: parseInt(refreshTokenObj["Max-Age"]),
+      path: refreshTokenObj.Path || "/",
+      sameSite: refreshTokenObj.SameSite,
+    });
+
+    return result;
   } catch (error) {
+    console.log(error);
+
     return { error: "Login failed" };
   }
 };
