373bcda6aaace2b20a2162ff2ff64f07ec63d9ca
dipongkorroy000 <dipongkorroy000@gmail.com>
2025-11-14 19:54:11 +0600

    registration form error handling task done

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -1,25 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
-import { useActionState, useEffect } from "react";
-import { Button } from "./ui/button";
-import { Field, FieldDescription, FieldGroup, FieldLabel } from "./ui/field";
-import { Input } from "./ui/input";
-import { registerPatient } from "@/services/auth/registerPatient";
-import { toast } from "sonner";
+import {useActionState, useEffect} from "react";
+import {Button} from "./ui/button";
+import {Field, FieldDescription, FieldGroup, FieldLabel} from "./ui/field";
+import {Input} from "./ui/input";
+import {registerPatient} from "@/services/auth/registerPatient";
+import {toast} from "sonner";
+import {getInputFieldError} from "@/lib/getInputFieldError";
 
 const RegisterForm = () => {
   const [state, formAction, isPending] = useActionState(registerPatient, null);
   // console.log(state, "state");
   // this is from data validation function -> before api call
-  const getFieldError = (fieldName: string) => {
-    if (state && state.errors) {
-      const error = state.errors.find((err: any) => err.field === fieldName);
-
-      if (error) return error?.message;
-      else return null;
-    } else return null;
-  };
 
   useEffect(() => {
     if (state && !state.success && state.message) toast.error(state.message);
@@ -33,7 +26,7 @@ const RegisterForm = () => {
           <Field>
             <FieldLabel htmlFor="name">Full Name</FieldLabel>
             <Input id="name" name="name" type="text" placeholder="John Doe" />
-            {getFieldError("name") && <FieldDescription className="text-red-600">{getFieldError("name")}</FieldDescription>}
+            {getInputFieldError("name", state) && <FieldDescription className="text-red-600">{getInputFieldError("name", state)}</FieldDescription>}
           </Field>
 
           {/* Address */}
@@ -41,7 +34,7 @@ const RegisterForm = () => {
             <FieldLabel htmlFor="address">Address</FieldLabel>
             <Input id="address" name="address" type="text" placeholder="123 Main St" />
 
-            {getFieldError("address") && <FieldDescription className="text-red-600">{getFieldError("address")}</FieldDescription>}
+            {getInputFieldError("address", state) && <FieldDescription className="text-red-600">{getInputFieldError("address", state)}</FieldDescription>}
           </Field>
 
           {/* Email */}
@@ -49,7 +42,7 @@ const RegisterForm = () => {
             <FieldLabel htmlFor="email">Email</FieldLabel>
             <Input id="email" name="email" type="email" placeholder="m@example.com" />
 
-            {getFieldError("email") && <FieldDescription className="text-red-600">{getFieldError("email")}</FieldDescription>}
+            {getInputFieldError("email", state) && <FieldDescription className="text-red-600">{getInputFieldError("email", state)}</FieldDescription>}
           </Field>
 
           {/* Password */}
@@ -57,7 +50,7 @@ const RegisterForm = () => {
             <FieldLabel htmlFor="password">Password</FieldLabel>
             <Input id="password" name="password" type="password" />
 
-            {getFieldError("password") && <FieldDescription className="text-red-600">{getFieldError("password")}</FieldDescription>}
+            {getInputFieldError("password", state) && <FieldDescription className="text-red-600">{getInputFieldError("password", state)}</FieldDescription>}
           </Field>
 
           {/* Confirm Password */}
@@ -65,8 +58,8 @@ const RegisterForm = () => {
             <FieldLabel htmlFor="confirmPassword">Confirm Password</FieldLabel>
             <Input id="confirmPassword" name="confirmPassword" type="password" />
 
-            {getFieldError("confirmPassword") && (
-              <FieldDescription className="text-red-600">{getFieldError("confirmPassword")}</FieldDescription>
+            {getInputFieldError("confirmPassword", state) && (
+              <FieldDescription className="text-red-600">{getInputFieldError("confirmPassword", state)}</FieldDescription>
             )}
           </Field>
         </div>
